57b523fe0c8382b3902144e33ebfae49de9755d8
Artyom <alchangyan@mail.ru>
2019-03-19 22:06:00 +0400

    added initial configuration for react

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,17 +1,82 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: "./src/index.js",
   output: {
-    "path": path(__dirname, 'build') ,
+    "path": path.join(__dirname, 'build'),
     filename: 'first-bundle.js'
+  },
+  /* Dev server */
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+    hot: true,
+    hotOnly: true,
+    open: true,
+    overlay: true,
+    port: 4000
   },
   module: {
     rules: [
+      /* JS */
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      /* Raw data */
       {
         test: /\.txt$/,
         use: 'raw-loader'
-      }
+      },
+      /* Styles */
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      /* Images */
+      {
+        test: /\.(png|svg|jp(e)g|gif|ico)$/,
+        use: [
+          'file-loader',
+        ]
+      },
+      /* Fonts */
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        // HTML LOADER
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      /* HTML loader */
+      /* {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'html-loader'
+        ]
+      } */
     ]
-  }
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin([{ from: 'static', to: 'static' }]),
+    new HtmlWebpackPlugin({template: './public/index.html'}),
+    new webpack.HotModuleReplacementPlugin()
+  ]
 }
